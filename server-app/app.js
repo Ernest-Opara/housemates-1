a7c7a6a19f95c9b9d623d11873ae1bc0915b83fa
Olutunde <solabi.tunde@gmail.com>
2021-09-20 09:08:10 +0000

    feat: added logging for api requests and parse for application/json

--- server-app/app.js
+++ server-app/app.js
@@ -3,13 +3,25 @@ dotenv.config()
 import express from 'express'
 import router from './routes/routes.js'
 import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(cors())
 app.use(express.static('public'))
+app.use(morgan('combined'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', router)
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+})
 
 mongoose.connect(
   process.env.MONGO_URI,
