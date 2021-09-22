07269e1b901b97b8cf4470ef66fa59d45a4b434a
jolinf <joshuaulinifun@gmail.com>
2021-09-22 16:07:16 +0100

    signup page

--- web-app/src/components/Signup.jsx
+++ web-app/src/components/Signup.jsx
@@ -2,7 +2,64 @@ import React from 'react'
 import Layout from '../shared/Layout'
 
 const Signup = () => {
-  return <Layout>{/* Write all your code here */}</Layout>
+
+  const handleSignUp = () =>{
+
+  }
+  return <Layout>{
+    <div className='login '>
+        <form>
+          <div className='cont d-flex flex-column justify-content-center'>
+            <div className='form sign-in'>
+             
+                <h2 className='text-secondary'>SIGN UP</h2>
+              
+
+              <label>
+                <span>First Name</span>
+                <input
+                  type='text'
+                  name='firstName'
+                  autoComplete='off'
+                />
+              </label>
+
+              <label>
+                <span>Last Name</span>
+                <input
+                  type='text'
+                  name='lName'
+                  autoComplete='off'
+                />
+              </label>
+
+              <label>
+                <span>Email</span>
+                <input
+                  type='email'
+                  name='email'
+                  autoComplete='off'
+                />
+              </label>
+              <label>
+                <span>Password</span>
+                <input
+                  type='password'
+                  name='password'
+                  autoComplete='off'
+                />
+              </label>
+              <button type='button' className='submit'>
+                Sign Up
+              </button>
+            </div>
+            <div className='sub-cont'>
+              <div className='img'></div>
+            </div>
+          </div>
+        </form>
+      </div>
+  }</Layout>
 }
 
 export default Signup
